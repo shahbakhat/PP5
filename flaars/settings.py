2b032b0516767a25e1bacf953f0246e918c38919
Shah Bakhat <shahbakhat008@gmail.com>
2023-08-15 19:57:21 +0100

    created base template and populated it

--- flaars/settings.py
+++ flaars/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,10 @@ ROOT_URLCONF = 'flaars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR,'flaars', 'templates'),
+            os.path.join(BASE_DIR, 'flaars','templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
